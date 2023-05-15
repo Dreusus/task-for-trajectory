d16742cdc57f531021c7ff84aee9de123550414d
unknown <pitakxdd@gmail.com>
2023-05-15 17:15:40 +0300

    Create component Footer

---
+++ src/components/CarsList/CarsList.js
@@ -0,0 +1,6 @@
+function CarsList() {
+  return (
+    <div>123</div>
+  )
+}
+export default CarsList;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+
+function Footer()  {
+
+  return  (
+    <footer className="footer">
+
+    <div className="footer__title">
+      Тестовое задания на Frontend-разработчика
+    </div>
+
+    <div className="footer__container">
+      <div className="footer__copy">&#169; 2023</div>
+      <ul className="footer__socials">
+        <li><a className="footer__social" href="https://github.com/Dreusus">Github</a></li>
+        <li><a className="footer__social" href="https://glonassgps.biz/">&#169; Траектория</a></li>
+      </ul>
+    </div>
+
+  </footer>
+  )
+}
+
+export default Footer
